febd9238336fffea0aac08d4f024675c394d502f
benz038 <leods.official@gmail.com>
2026-05-22 09:24:40 +0530

    Add GitHub Actions workflow and .gitignore for Firebase secrets

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCM2khcveCcRGm54KkKQKu-U1xic665z3g",
-  authDomain: "dsa-progress-tracker-fcc73.firebaseapp.com",
-  projectId: "dsa-progress-tracker-fcc73",
-  storageBucket: "dsa-progress-tracker-fcc73.firebasestorage.app",
-  messagingSenderId: "15519234389",
-  appId: "1:15519234389:web:0bbe6ec17bdf09d79557dc"
-};
