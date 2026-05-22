b6e808dd425815aa9adf38ec45f524cdb6915787
benz038 <leods.official@gmail.com>
2026-05-22 09:20:32 +0530

    Add Firebase configuration credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCM2khcveCcRGm54KkKQKu-U1xic665z3g",
+  authDomain: "dsa-progress-tracker-fcc73.firebaseapp.com",
+  projectId: "dsa-progress-tracker-fcc73",
+  storageBucket: "dsa-progress-tracker-fcc73.firebasestorage.app",
+  messagingSenderId: "15519234389",
+  appId: "1:15519234389:web:0bbe6ec17bdf09d79557dc"
 };
